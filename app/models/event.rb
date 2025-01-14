a5604e8d9ac2a38d9b221a7151cdc622ed29867b
Akram Abdelnasser <akram.abdelnasser@lunedata.io>
2025-01-14 18:19:13 +0400

    Handle case when updating the number of tickets of an event to respect the remainig tickets

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,9 @@ class Event < ApplicationRecord
   validates :location, presence: true
   validates :start_time, presence: true
   validates :total_tickets, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :remaining_tickets, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :start_time_must_be_in_future
+  validate :validate_total_tickets
 
   # SCOPES #
   scope :future_events, -> { where('start_time > ?', Time.now) }
@@ -18,7 +20,7 @@ class Event < ApplicationRecord
   has_many :booked_users, through: :bookings, source: :user
 
   before_create :set_remaining_tickets
-  
+
   private
 
   def set_remaining_tickets
@@ -28,6 +30,12 @@ class Event < ApplicationRecord
   def start_time_must_be_in_future
     if start_time.present? && start_time <= Time.now
       errors.add(:start_time, "must be in the future")
+    end
+  end
+
+  def validate_total_tickets
+    if total_tickets < remaining_tickets
+      errors.add(:total_tickets, "must be greater than or equal to the remaining tickets")
     end
   end
 end
